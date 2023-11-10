7cc6a297b8a10e76df66b9f9c16018c63eb446fd
mayankmehar9128 <150352254+mayankmehar9128@users.noreply.github.com>
2023-11-10 11:08:49 +0000

    datatypes

---
+++ javascript basics/Vereabe.js
@@ -0,0 +1,16 @@
+console.log("Mayank Mehar");
+
+const accountId = 112233
+let emailId = "mathyu@gmail.com"
+var password = "Mike";
+accountHolder = "Mike";
+
+let accountstate
+
+
+/*
+prefer not to use var
+because of issue in functional scope and block scope
+*/
+
+console.table([accountId, accountHolder, password, emailId, accountstate])
