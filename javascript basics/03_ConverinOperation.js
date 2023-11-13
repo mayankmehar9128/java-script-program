a64cd76a1f3c095e0f9c907dc6fdce36a0e65564
mayankmehar9128 <150352254+mayankmehar9128@users.noreply.github.com>
2023-11-13 15:49:17 +0000

    Conversion of data types

---
+++ javascript basics/03_ConverinOperation.js
@@ -0,0 +1,41 @@
+/* let score = 33 */
+
+let score = "33"
+
+let scoreA = "33abc"
+
+//console.log(score);
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let convertInNumber = Number(score);
+
+
+let convertInNumberA = Number(scoreA);
+
+
+//console.log(convertInNumber);
+
+//console.log(typeof(convertInNumber));
+
+//console.log(convertInNumberA);
+
+
+// "33" => 33
+//"33abc" => NaN 
+// null => 0
+//true => 1; false => 0
+
+let isLoggedIn = 1
+
+let isLoggedIn2 = "mayank"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+
+console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn2);
+
+// 1 => true; o => false
+// "" => false
+// "mayank" => true
