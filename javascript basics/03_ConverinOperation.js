ba3bc2482e81678b144081862e34acb9aff3e216
mayankmehar9128 <150352254+mayankmehar9128@users.noreply.github.com>
2023-11-14 04:39:23 +0000

    Comparison

--- javascript basics/03_ConverinOperation.js
+++ javascript basics/03_ConverinOperation.js
@@ -39,3 +39,49 @@ console.log(booleanIsLoggedIn2);
 // 1 => true; o => false
 // "" => false
 // "mayank" => true
+
+
+// ******************operation****************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "mayank"
+let str2 = " mehar"
+
+let str3 = str1 +str2
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 101
+gameCounter++ // postfix
+// ++gameCounter //prefix
+// console.log(gameCounter); // same output with prefix
+
+// link to study
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x = 3;
+const y = x++;
+
+console.log(y);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(a, b);
+// Expected output: "a:4, b:4"
+
---
+++ javascript basics/04_comparision.js
@@ -0,0 +1,23 @@
+// basic conversion
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" >1 );    
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log((null == 0));      // avoid this type of conversion
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);    // avoid this type of conversion
+console.log(undefined <0);
+
+// === check the data type also
+
+console.log("2" === 2);
