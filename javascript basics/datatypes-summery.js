a3fa69d39d24180c3bd97d085750471e5e15b425
mayankmehar9128 <150352254+mayankmehar9128@users.noreply.github.com>
2023-11-14 05:06:38 +0000

    Data type Summery

---
+++ javascript basics/datatypes-summery.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;     // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3465323361435562114n  // bigInt data type
+
+// Referene (Non primitve)
+
+// Array, Objects, Functons
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "mayank", 
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
